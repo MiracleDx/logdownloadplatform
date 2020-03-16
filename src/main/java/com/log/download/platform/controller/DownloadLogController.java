43c108941d64daf738545eaf804ebb210ea143fa
Mintcat <761042759@qq.com>
2020-03-16 09:08:55 +0800

    下载方法

---
+++ src/main/java/com/log/download/platform/controller/DownloadLogController.java
@@ -0,0 +1,95 @@
+package com.log.download.platform.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+@RestController
+public class DownloadLogController {
+
+
+    @RequestMapping("download")
+    public HttpServletResponse download(String path, HttpServletResponse response) {
+        try {
+            path = "d:/test.log";
+            // path是指欲下载的文件的路径。
+            File file = new File(path);
+            // 取得文件名。
+            String filename = file.getName();
+            // 取得文件的后缀名。
+            String ext = filename.substring(filename.lastIndexOf(".") + 1).toUpperCase();
+
+            // 以流的形式下载文件。
+            InputStream fis = new BufferedInputStream(new FileInputStream(path));
+            byte[] buffer = new byte[fis.available()];
+            fis.read(buffer);
+            fis.close();
+            // 清空response
+            response.reset();
+            // 设置response的Header
+            response.addHeader("Content-Disposition", "attachment;filename=" + new String(filename.getBytes()));
+            response.addHeader("Content-Length", "" + file.length());
+            OutputStream toClient = new BufferedOutputStream(response.getOutputStream());
+            response.setContentType("application/octet-stream");
+            toClient.write(buffer);
+            toClient.flush();
+            toClient.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return response;
+    }
+
+    //@RequestMapping("download")
+    public void downloadLocal(HttpServletResponse response) throws FileNotFoundException {
+        // 下载本地文件
+        String fileName = "Operator.doc".toString(); // 文件的默认保存名
+        // 读到流中
+        InputStream inStream = new FileInputStream("d:/Operator.doc");// 文件的存放路径
+        // 设置输出的格式
+        response.reset();
+        response.setContentType("bin");
+        response.addHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+        // 循环取出流中的数据
+        byte[] b = new byte[100];
+        int len;
+        try {
+            while ((len = inStream.read(b)) > 0)
+                response.getOutputStream().write(b, 0, len);
+            inStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public File download (HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //提供下载文件前进行压缩，即服务端生成压缩文件 File file = new File(zipPath);
+        File file = new File("");
+        FileOutputStream fos = new FileOutputStream(file);
+        //1.获取要下载的文件的绝对路径
+        String realPath = "";
+        //2.获取要下载的文件名
+        String fileName = realPath.substring(realPath.lastIndexOf(File.separator)+1);
+        response.reset();
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/octet-stream");
+        //3.设置content-disposition响应头控制浏览器以下载的形式打开文件
+        response.addHeader("Content-Disposition","attachment;filename=" + new String(fileName.getBytes(),"utf-8"));
+        //获取文件输入流
+        InputStream in = new FileInputStream(realPath);
+        int len = 0; byte[] buffer = new byte[1024];
+        OutputStream out = response.getOutputStream();
+        while ((len = in.read(buffer)) > 0) {
+            //将缓冲区的数据输出到客户端浏览器
+            out.write(buffer,0,len);
+        } in.close(); return file;
+
+    }
+
+
+}
