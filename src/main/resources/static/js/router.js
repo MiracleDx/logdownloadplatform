0523ca28aae74b1720b0ebbb7bc8097b5f05b3ad
dongx <775422441@qq.com>
2020-03-16 16:32:18 +0800

    feat: 增加router.js

---
+++ src/main/resources/static/js/router.js
@@ -0,0 +1,7 @@
+const remote = "http://localhost:8080/"
+const router = {
+    "queryLogDetails": remote + "queryLogDetails",
+    "getMenu": remote + "getMenu",
+    "upload": remote + "upload",
+    "download": remote + "download"
+}
