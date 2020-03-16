38af538f45b8e843d8b6601c72d85efcaf06aa5c
dongx <775422441@qq.com>
2020-03-16 16:14:33 +0800

    feat: 增加日志详情实体类和日志详情视图对象

---
+++ src/main/java/com/log/download/platform/entity/LogDetail.java
@@ -0,0 +1,19 @@
+package com.log.download.platform.entity;
+
+import lombok.Data;
+
+/**
+ * LogDetail
+ * 日志详情实体类
+ * @author Dongx
+ * Description:
+ * Created in: 2020-03-16 15:56
+ * Modified by:
+ */
+@Data
+public class LogDetail {
+	
+	private Integer id;
+	
+	private String path;
+}
---
+++ src/main/java/com/log/download/platform/vo/LogDetailVO.java
@@ -0,0 +1,19 @@
+package com.log.download.platform.vo;
+
+import lombok.Data;
+
+/**
+ * LogDetailVO
+ * 
+ * @author Dongx
+ * Description:
+ * Created in: 2020-03-16 16:12
+ * Modified by:
+ */
+@Data
+public class LogDetailVO {
+
+	private Integer id;
+
+	private String path;
+}
