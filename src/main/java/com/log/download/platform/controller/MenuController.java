387620a79fae90fd6f0791e1229b23d7e28b1933
dongx <775422441@qq.com>
2020-03-18 10:45:19 +0800

    feat: 增加菜单服务类调用

--- src/main/java/com/log/download/platform/controller/MenuController.java
+++ src/main/java/com/log/download/platform/controller/MenuController.java
@@ -1,11 +1,13 @@
 package com.log.download.platform.controller;
 
 import com.log.download.platform.response.ServerResponse;
+import com.log.download.platform.service.MenuService;
 import com.log.download.platform.vo.MenuVO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -16,20 +18,19 @@ import java.util.List;
  * Created in: 2020-03-16 9:40
  * Modified by:
  */
+@Slf4j
 @RestController
 public class MenuController {
 
-	/**
-	 * 菜单树
-	 */
-	public List<MenuVO> menu = new ArrayList<>();
-	
+	@Resource
+	private MenuService menuService;
 	
 	@GetMapping("/getMenu")
 	public ServerResponse<List<MenuVO>> getMenu() {
-		if (menu == null || menu.size() == 0) {
+		if (menuService.menu == null || menuService.menu.size() == 0) {
+			log.error("菜单加载失败，请联系部署组，上传正确的日志清单文件");
 			return ServerResponse.failure("菜单加载失败，请联系部署组，上传正确的日志清单文件");
 		}
-		return ServerResponse.success(menu);
+		return ServerResponse.success(menuService.menu);
 	}
 }
