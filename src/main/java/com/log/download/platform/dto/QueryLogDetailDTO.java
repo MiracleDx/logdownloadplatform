24ed2c954c7db64b6916781c326b59ed4167c1a0
dongx <775422441@qq.com>
2020-03-18 15:31:04 +0800

    feat: 增加日志查询DTO

---
+++ src/main/java/com/log/download/platform/dto/QueryLogDetailDTO.java
@@ -0,0 +1,25 @@
+package com.log.download.platform.dto;
+
+import lombok.Data;
+
+/**
+ * QueryLogDetailDTO
+ *
+ * @author Dongx
+ * Description:
+ * Created in: 2020-03-18 14:08
+ * Modified by:
+ */
+@Data 
+public class QueryLogDetailDTO {
+
+	/**
+	 * 服务编号
+	 */
+	private String label;
+
+	/**
+	 * ip集合
+	 */
+	private String[] ips;
+}
