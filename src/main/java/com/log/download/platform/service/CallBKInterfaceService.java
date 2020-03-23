848222f245ee9af317c8c3a0f181ece27e8d0aee
dongx <775422441@qq.com>
2020-03-23 11:27:00 +0800

    fix: 修改入参json

--- src/main/java/com/log/download/platform/service/CallBKInterfaceService.java
+++ src/main/java/com/log/download/platform/service/CallBKInterfaceService.java
@@ -129,9 +129,9 @@ public class CallBKInterfaceService {
                 "\t\"bk_app_secret\": \"" + bk_app_secret + "\",\n" +
                 "\t\"bk_username\": \"" + bk_username + "\",\n" +
                 "\t\"bk_biz_id\": " + bk_biz_id + ",\n" +
-                "\t\"file_target_path\": \"/tmp/0_"+ip+"/"+path.substring(0,end)+"\",\n" +
+                "\t\"file_target_path\": \"/tmp/0_" + ip + "/" + path.substring(0, end) + "\",\n" +
                 "\t\"account\": \"root\",\n" +
-                "\t\"ip_list\": \"[{\"bk_cloud_id\": 0,\"ip\": \"10.155.27.48\"}]\",\n" +
+                "\t\"ip_list\": [{\"bk_cloud_id\": 0,\"ip\": \"10.155.27.48\"}],\n" +
                 "\t\"file_source\": [{\n" +
                 "\t\t\"files\":[\"" + path + "\"],\n" +
                 "\t\t\"account\": \"ubuntu\",\n" +
