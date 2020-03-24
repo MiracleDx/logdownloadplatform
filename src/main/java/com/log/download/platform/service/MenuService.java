f3a0e496a3aaed86dba66e25de88caea260d7f64
dongx <775422441@qq.com>
2020-03-24 15:45:57 +0800

    feat: 增加判断是否属于统一命名空间

--- src/main/java/com/log/download/platform/service/MenuService.java
+++ src/main/java/com/log/download/platform/service/MenuService.java
@@ -131,53 +131,59 @@ public class MenuService {
 			// 从一级菜单中获取对应的二级菜单
 			List<DeploymentGroupBO> seconds = firstMenu.get(first.getLabel());
 			// 增加一级菜单的中文名称
+			String firstLabel = first.getLabel();
 			first.setLabel(first.getLabel() + "-" + seconds.get(0).getProjectName());
 			// 构造二级菜单
 			for (DeploymentGroupBO secondBO : seconds) {
-				MenuVO second = new MenuVO();
-				// 获取服务编码
-				String serverCode = secondBO.getApplicationName();
-				second.setLabel(serverCode + "-" + secondBO.getServerName());
-				// 从二级菜单中获取三级菜单
-				List<DeploymentGroupBO> thirdMenu = secondMenu.get(serverCode);
-				List<MenuVO> thirds = new ArrayList<>();
-				// 构造三级菜单
-				for (DeploymentGroupBO thirdBO : thirdMenu) {
-					MenuVO third = new MenuVO();
-					String group = thirdBO.getGroup();
-					// 取中心名称
-					String cluster = thirdBO.getCluster();
-					String[] strs = cluster.split("-");
-					if (strs != null && strs.length > 0) {
-						third.setCluster(strs[0]);
-					} else {
-						third.setCluster(cluster.substring(0, 2));
-					}
-					third.setLabel(group);
-					third.setChildren(new ArrayList<>());
-					third.setIps(groupMap.get(group).stream().map(DeploymentGroupBO::getIp).filter(StringUtils::isNoneBlank).collect(Collectors.toList()));
-					third.setBkParam(thirdBO.getNameSpace() + " " + thirdBO.getGroup());
-					
-					// 判断总/分公司
-					String[] split = group.split("-");
-					boolean flag;
-					// 普通中心
-					if (split.length == 4) {
-						flag = first.getLabel().contains(split[2]);
-						// ua 前台页面 前台应用
-					} else {
-						flag = true;
-					}
+				// 判断是否属于同一菜单 区分BJ GZ
+				if (StringUtils.equals(secondBO.getNameSpace(), firstLabel)) {
+					MenuVO second = new MenuVO();
+					// 获取服务编码
+					String serverCode = secondBO.getApplicationName();
+					second.setLabel(serverCode + "-" + secondBO.getServerName());
+					// 从二级菜单中获取三级菜单
+					List<DeploymentGroupBO> thirdMenu = secondMenu.get(serverCode);
+					List<MenuVO> thirds = new ArrayList<>();
+					// 构造三级菜单
+					for (DeploymentGroupBO thirdBO : thirdMenu) {
+						if (StringUtils.equals(thirdBO.getNameSpace(), firstLabel)) {
+							MenuVO third = new MenuVO();
+							String group = thirdBO.getGroup();
+							// 取中心名称
+							String cluster = thirdBO.getCluster();
+							String[] strs = cluster.split("-");
+							if (strs != null && strs.length > 0) {
+								third.setCluster(strs[0]);
+							} else {
+								third.setCluster(cluster.substring(0, 2));
+							}
+							third.setLabel(group);
+							third.setChildren(new ArrayList<>());
+							third.setIps(groupMap.get(group).stream().map(DeploymentGroupBO::getIp).filter(StringUtils::isNoneBlank).collect(Collectors.toList()));
+							third.setBkParam(thirdBO.getNameSpace() + " " + thirdBO.getGroup());
 
-					if (!thirds.contains(third) && flag) {
-						thirds.add(third);
-					}
-				}
-				second.setChildren(thirds);
+							// 判断总/分公司
+							String[] split = group.split("-");
+							boolean flag;
+							// 普通中心
+							if (split.length == 4) {
+								flag = first.getLabel().contains(split[2]);
+								// ua 前台页面 前台应用
+							} else {
+								flag = true;
+							}
 
-				// 一级菜单中不包含二级时添加
-				if (!first.getChildren().contains(second)) {
-					first.getChildren().add(second);
+							if (!thirds.contains(third) && flag) {
+								thirds.add(third);
+							}
+						}
+					}
+					second.setChildren(thirds);
+	
+					// 一级菜单中不包含二级时添加
+					if (!first.getChildren().contains(second)) {
+						first.getChildren().add(second);
+					}
 				}
 			}
 		}
