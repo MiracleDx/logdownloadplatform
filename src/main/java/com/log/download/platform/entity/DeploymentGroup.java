44af90e92742039012d299db9b583ae4a65f61b4
Dongx <775422441@qq.com>
2020-04-17 16:30:44 +0800

    feat: 修改模版文件加载方式

--- src/main/java/com/log/download/platform/entity/DeploymentGroup.java
+++ src/main/java/com/log/download/platform/entity/DeploymentGroup.java
@@ -15,56 +15,51 @@ import lombok.Data;
 public class DeploymentGroup {
 
 	/**
-	 * 项目编号
+	 * 命名空间
 	 */
-	@ExcelProperty(value = "项目编号", index = 0)
-	private String projectNo;
-
+	@ExcelProperty(value = "命名空间", index = 0)
+	private String nameSpace;
+	
 	/**
 	 * 项目名称
 	 */
-	@ExcelProperty(value = "项目名称", index = 1)
+	@ExcelProperty(value = "命名空间描述", index = 1)
 	private String projectName;
-
-	/**
-	 * 微服务/前台页面/前台应用名称
-	 */
-	@ExcelProperty(value = "微服务/前台页面/前台应用名称", index = 2)
-	private String serverName;
-
-	/**
-	 * 地域
-	 */
-	@ExcelProperty(value = "地域", index = 3)
-	private String area;
-
-	/**
-	 * 集群
-	 */
-	@ExcelProperty(value = "集群", index = 4)
-	private String cluster;
-
-	/**
-	 * 命名空间
-	 */
-	@ExcelProperty(value = "命名空间", index = 5)
-	private String nameSpace;
 
 	/**
 	 * 应用名称
 	 */
-	@ExcelProperty(value = "应用名称", index = 6)
+	@ExcelProperty(value = "应用名称", index = 2)
 	private String applicationName;
+
+	/**
+	 * 微服务/前台页面/前台应用名称
+	 */
+	@ExcelProperty(value = "应用描述", index = 3)
+	private String serverName;
 
 	/**
 	 * 部署组名称
 	 */
-	@ExcelProperty(value = "部署组名称", index = 7)
+	@ExcelProperty(value = "部署组名称", index = 4)
 	private String group;
+
+	/**
+	 * 集群
+	 */
+	@ExcelProperty(value = "集群名称", index = 5)
+	private String cluster;
+
+
+	/**
+	 * 地域
+	 */
+	@ExcelProperty(value = "区域", index = 6)
+	private String area;
 
 	/**
 	 * 实例所在节点IP
 	 */
-	@ExcelProperty(value = "实例所在节点IP", index = 8)
+	@ExcelProperty(value = "虚机IP", index = 7)
 	private String ip;
 }
--- src/main/java/com/log/download/platform/service/MenuService.java
+++ src/main/java/com/log/download/platform/service/MenuService.java
@@ -2,12 +2,14 @@ package com.log.download.platform.service;
 
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelReader;
+import com.alibaba.excel.read.metadata.ReadSheet;
 import com.log.download.platform.bo.DeploymentGroupBO;
 import com.log.download.platform.bo.GatewayGroupBO;
 import com.log.download.platform.entity.DeploymentGroup;
 import com.log.download.platform.entity.GatewayGroup;
 import com.log.download.platform.support.UploadDeploymentGroupListener;
 import com.log.download.platform.support.UploadGatewayGroupListener;
+import com.log.download.platform.vo.LogDetailVO;
 import com.log.download.platform.vo.MenuVO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -20,6 +22,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
@@ -46,52 +49,119 @@ public class MenuService {
 	 * @return
 	 * @throws IOException
 	 */
+	//public List<MenuVO> getMenu(InputStream in) {
+	//	ExcelReader excelReader = EasyExcel.read(in).build();
+	//
+	//	// 读取sheet1
+	//	UploadDeploymentGroupListener uploadDeploymentGroupListener = new UploadDeploymentGroupListener();
+	//	// 读取sheet2
+	//	UploadGatewayGroupListener uploadGatewayGroupListener = new UploadGatewayGroupListener();
+	//
+	//	// 获取sheet页信息
+	//	CompletableFuture
+	//			.supplyAsync(() -> EasyExcel.readSheet(0).head(DeploymentGroup.class).registerReadListener(uploadDeploymentGroupListener).build())
+	//			.thenAcceptBoth(CompletableFuture
+	//					.supplyAsync(() -> EasyExcel.readSheet(1).head(GatewayGroup.class).registerReadListener(uploadGatewayGroupListener).build()), (sheet1, sheet2) -> {
+	//				excelReader.read(sheet1, sheet2);
+	//			}).thenRun(() -> {
+	//				// 这里千万别忘记关闭，读的时候会创建临时文件，到时磁盘会崩的
+	//				excelReader.finish();
+	//				try {
+	//					in.close();
+	//				} catch (IOException e) {
+	//					e.printStackTrace();
+	//				}
+	//			}).join();
+	//
+	//	// 获取aop代理对象
+	//	MenuService o = (MenuService) AopContext.currentProxy();
+	//	
+	//	// 转换菜单树
+	//	CompletableFuture.supplyAsync(() -> {
+	//		// 转换BO
+	//		List<DeploymentGroupBO> list = uploadDeploymentGroupListener.getList().stream().map(e -> {
+	//			DeploymentGroupBO bo = new DeploymentGroupBO();
+	//			BeanUtils.copyProperties(e, bo);
+	//			return bo;
+	//		}).collect(Collectors.toList());
+	//		// 获得微服务菜单
+	//		return convertServer2Tree(list);
+	//	}).thenAcceptBoth(CompletableFuture.supplyAsync(() -> {
+	//		// 转换BO
+	//		List<GatewayGroupBO> list = uploadGatewayGroupListener.getList().stream().map(e -> {
+	//			GatewayGroupBO bo = new GatewayGroupBO();
+	//			BeanUtils.copyProperties(e, bo);
+	//			return bo;
+	//		}).collect(Collectors.toList());
+	//		// 获得微服务网关菜单
+	//		return convertGateway2Tree(list);
+	//	}), (d, g) -> {
+	//		log.info("微服务菜单树转换成功");
+	//
+	//		if (o.menu == null || o.menu.size() == 0) {
+	//			o.menu = new ArrayList<>();
+	//		}
+	//
+	//		// 清空缓存数据
+	//		if (o.menu.size() >= 2) {
+	//			o.menu.clear();
+	//		}
+	//
+	//		MenuVO server = new MenuVO();
+	//		server.setLabel("微服务");
+	//		server.setChildren(d);
+	//		o.menu.add(server);
+	//
+	//		MenuVO gateway = new MenuVO();
+	//		gateway.setLabel("微服务网关");
+	//		gateway.setChildren(g);
+	//		o.menu.add(gateway);
+	//	}).join();
+	//
+	//	//Stream.iterate(0, i -> i + 1).limit(bos.size()).forEach(i -> bos.get(i).setId(i));
+	//	return menu;
+	//}
+
 	public List<MenuVO> getMenu(InputStream in) {
 		ExcelReader excelReader = EasyExcel.read(in).build();
 
-		// 读取sheet1
+		// 读取sheet
 		UploadDeploymentGroupListener uploadDeploymentGroupListener = new UploadDeploymentGroupListener();
-		// 读取sheet2
-		UploadGatewayGroupListener uploadGatewayGroupListener = new UploadGatewayGroupListener();
 
-		// 获取sheet页信息
-		CompletableFuture
-				.supplyAsync(() -> EasyExcel.readSheet(0).head(DeploymentGroup.class).registerReadListener(uploadDeploymentGroupListener).build())
-				.thenAcceptBoth(CompletableFuture
-						.supplyAsync(() -> EasyExcel.readSheet(1).head(GatewayGroup.class).registerReadListener(uploadGatewayGroupListener).build()), (sheet1, sheet2) -> {
-					excelReader.read(sheet1, sheet2);
-				}).thenRun(() -> {
-					// 这里千万别忘记关闭，读的时候会创建临时文件，到时磁盘会崩的
-					excelReader.finish();
-					try {
-						in.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}).join();
+		ReadSheet sheet = EasyExcel.readSheet(0).head(DeploymentGroup.class).registerReadListener(uploadDeploymentGroupListener).build();
+		excelReader.read(sheet);
+		excelReader.finish();
+		try {
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Map<Boolean, List<DeploymentGroup>> collect = uploadDeploymentGroupListener.getList()
+				.parallelStream().collect(Collectors.partitioningBy(data -> data.getApplicationName().contains("msgw")));
 
 		// 获取aop代理对象
 		MenuService o = (MenuService) AopContext.currentProxy();
-		
+
 		// 转换菜单树
 		CompletableFuture.supplyAsync(() -> {
 			// 转换BO
-			List<DeploymentGroupBO> list = uploadDeploymentGroupListener.getList().stream().map(e -> {
+			List<DeploymentGroupBO> list = collect.get(false).stream().map(e -> {
 				DeploymentGroupBO bo = new DeploymentGroupBO();
 				BeanUtils.copyProperties(e, bo);
 				return bo;
 			}).collect(Collectors.toList());
 			// 获得微服务菜单
-			return convertServer2Tree(list);
+			return convert2Tree(list);
 		}).thenAcceptBoth(CompletableFuture.supplyAsync(() -> {
 			// 转换BO
-			List<GatewayGroupBO> list = uploadGatewayGroupListener.getList().stream().map(e -> {
-				GatewayGroupBO bo = new GatewayGroupBO();
+			List<DeploymentGroupBO> list = collect.get(true).stream().map(e -> {
+				DeploymentGroupBO bo = new DeploymentGroupBO();
 				BeanUtils.copyProperties(e, bo);
 				return bo;
 			}).collect(Collectors.toList());
 			// 获得微服务网关菜单
-			return convertGateway2Tree(list);
+			return convert2Tree(list);
 		}), (d, g) -> {
 			log.info("微服务菜单树转换成功");
 
@@ -114,17 +184,17 @@ public class MenuService {
 			gateway.setChildren(g);
 			o.menu.add(gateway);
 		}).join();
-
-		//Stream.iterate(0, i -> i + 1).limit(bos.size()).forEach(i -> bos.get(i).setId(i));
+		
 		return menu;
 	}
+	
 
 	/**
 	 * 微服务菜单树生成
 	 * @param bos
 	 * @return
 	 */
-	private List<MenuVO> convertServer2Tree(List<DeploymentGroupBO> bos) {
+	private List<MenuVO> convert2Tree(List<DeploymentGroupBO> bos) {
 		// 一级菜单树
 		List<MenuVO> vos = new ArrayList<>();
 		for (DeploymentGroupBO bo : bos) {
@@ -148,7 +218,13 @@ public class MenuService {
 			List<DeploymentGroupBO> seconds = firstMenu.get(first.getLabel());
 			// 增加一级菜单的中文名称
 			String firstLabel = first.getLabel();
-			first.setLabel(first.getLabel() + "-" + seconds.get(0).getProjectName());
+			
+			if (StringUtils.isBlank(seconds.get(0).getProjectName())) {
+				first.setLabel(first.getLabel());
+			} else {
+				first.setLabel(first.getLabel() + "-" + seconds.get(0).getProjectName());
+			}
+			
 			// 构造二级菜单
 			for (DeploymentGroupBO secondBO : seconds) {
 				// 判断是否属于同一菜单 区分BJ GZ
@@ -156,7 +232,13 @@ public class MenuService {
 					MenuVO second = new MenuVO();
 					// 获取服务编码
 					String serverCode = secondBO.getApplicationName();
-					second.setLabel(serverCode + "-" + secondBO.getServerName());
+					
+					if (StringUtils.isBlank(secondBO.getServerName())) {
+						second.setLabel(serverCode);
+					} else {
+						second.setLabel(serverCode + "-" + secondBO.getServerName());
+					}
+					
 					// 从二级菜单中获取三级菜单
 					List<DeploymentGroupBO> thirdMenu = secondMenu.get(serverCode);
 					List<MenuVO> thirds = new ArrayList<>();
@@ -216,79 +298,79 @@ public class MenuService {
 	 * @param bos
 	 * @return
 	 */
-	private List<MenuVO> convertGateway2Tree(List<GatewayGroupBO> bos) {
-		// 一级菜单树
-		List<MenuVO> vos = new ArrayList<>();
-		for (GatewayGroupBO bo : bos) {
-			MenuVO vo = new MenuVO();
-			vo.setLabel(bo.getProjectNo());
-			vo.setChildren(new ArrayList<>());
-			if (!vos.contains(vo)) {
-				vos.add(vo);
-			}
-		}
-		// 获取一级菜单
-		Map<String, List<GatewayGroupBO>> firstMenu = bos.stream().collect(Collectors.groupingBy(GatewayGroupBO::getProjectNo));
-		// 获取二级菜单
-		Map<String, List<GatewayGroupBO>> secondMenu = bos.stream().collect(Collectors.groupingBy(GatewayGroupBO::getApplicationName));
-		// 获取各部署组对应的IP清单
-		Map<String, List<GatewayGroupBO>> groupMap = bos.stream().collect(Collectors.groupingBy(GatewayGroupBO::getCluster));
-
-		// 二级菜单树
-		for (MenuVO first : vos) {
-			// 从一级菜单中获取对应的二级菜单
-			List<GatewayGroupBO> seconds = firstMenu.get(first.getLabel());
-			// 增加一级菜单的中文名称
-			first.setLabel(first.getLabel() + "-" + seconds.get(0).getProjectName());
-			// 构造二级菜单
-			for (GatewayGroupBO secondBO : seconds) {
-				MenuVO second = new MenuVO();
-				// 获取服务编码
-				String serverCode = secondBO.getApplicationName();
-				second.setLabel(serverCode + "-" + secondBO.getServerName());
-				// 从二级菜单中获取三级菜单
-				List<GatewayGroupBO> thirdMenu = secondMenu.get(serverCode);
-				List<MenuVO> thirds = new ArrayList<>();
-				// 构造三级菜单
-				for (GatewayGroupBO thirdBO : thirdMenu) {
-					MenuVO third = new MenuVO();
-					String group = thirdBO.getCluster();
-					// 取中心名称
-					String cluster = thirdBO.getCluster();
-					String[] strs = cluster.split("-");
-					if (strs != null && strs.length > 0) {
-						third.setCluster(strs[0]);
-					} else {
-						third.setCluster(cluster.substring(0, 2));
-					}
-					third.setLabel(group);
-					third.setChildren(new ArrayList<>());
-					third.setIps(groupMap.get(group).stream().map(GatewayGroupBO::getIp).filter(StringUtils::isNoneBlank).collect(Collectors.toList()));
-					third.setBkParam(thirdBO.getApplicationName() + " " + thirdBO.getApplicationName());
-
-					// 判断总/分公司
-					String[] split = group.split("-");
-					boolean flag;
-					// 普通中心
-					if (split.length == 4) {
-						flag = first.getLabel().contains(split[2]);
-						// ua 前台页面 前台应用
-					} else {
-						flag = true;
-					}
-
-					if (!thirds.contains(third) && flag) {
-						thirds.add(third);
-					}
-				}
-				second.setChildren(thirds);
-
-				// 一级菜单中不包含二级时添加
-				if (!first.getChildren().contains(second)) {
-					first.getChildren().add(second);
-				}
-			}
-		}
-		return vos;
-	}
+	//private List<MenuVO> convertGateway2Tree(List<GatewayGroupBO> bos) {
+	//	// 一级菜单树
+	//	List<MenuVO> vos = new ArrayList<>();
+	//	for (GatewayGroupBO bo : bos) {
+	//		MenuVO vo = new MenuVO();
+	//		vo.setLabel(bo.getProjectNo());
+	//		vo.setChildren(new ArrayList<>());
+	//		if (!vos.contains(vo)) {
+	//			vos.add(vo);
+	//		}
+	//	}
+	//	// 获取一级菜单
+	//	Map<String, List<GatewayGroupBO>> firstMenu = bos.stream().collect(Collectors.groupingBy(GatewayGroupBO::getProjectNo));
+	//	// 获取二级菜单
+	//	Map<String, List<GatewayGroupBO>> secondMenu = bos.stream().collect(Collectors.groupingBy(GatewayGroupBO::getApplicationName));
+	//	// 获取各部署组对应的IP清单
+	//	Map<String, List<GatewayGroupBO>> groupMap = bos.stream().collect(Collectors.groupingBy(GatewayGroupBO::getCluster));
+	//
+	//	// 二级菜单树
+	//	for (MenuVO first : vos) {
+	//		// 从一级菜单中获取对应的二级菜单
+	//		List<GatewayGroupBO> seconds = firstMenu.get(first.getLabel());
+	//		// 增加一级菜单的中文名称
+	//		first.setLabel(first.getLabel() + "-" + seconds.get(0).getProjectName());
+	//		// 构造二级菜单
+	//		for (GatewayGroupBO secondBO : seconds) {
+	//			MenuVO second = new MenuVO();
+	//			// 获取服务编码
+	//			String serverCode = secondBO.getApplicationName();
+	//			second.setLabel(serverCode + "-" + secondBO.getServerName());
+	//			// 从二级菜单中获取三级菜单
+	//			List<GatewayGroupBO> thirdMenu = secondMenu.get(serverCode);
+	//			List<MenuVO> thirds = new ArrayList<>();
+	//			// 构造三级菜单
+	//			for (GatewayGroupBO thirdBO : thirdMenu) {
+	//				MenuVO third = new MenuVO();
+	//				String group = thirdBO.getCluster();
+	//				// 取中心名称
+	//				String cluster = thirdBO.getCluster();
+	//				String[] strs = cluster.split("-");
+	//				if (strs != null && strs.length > 0) {
+	//					third.setCluster(strs[0]);
+	//				} else {
+	//					third.setCluster(cluster.substring(0, 2));
+	//				}
+	//				third.setLabel(group);
+	//				third.setChildren(new ArrayList<>());
+	//				third.setIps(groupMap.get(group).stream().map(GatewayGroupBO::getIp).filter(StringUtils::isNoneBlank).collect(Collectors.toList()));
+	//				third.setBkParam(thirdBO.getApplicationName() + " " + thirdBO.getApplicationName());
+	//
+	//				// 判断总/分公司
+	//				String[] split = group.split("-");
+	//				boolean flag;
+	//				// 普通中心
+	//				if (split.length == 4) {
+	//					flag = first.getLabel().contains(split[2]);
+	//					// ua 前台页面 前台应用
+	//				} else {
+	//					flag = true;
+	//				}
+	//
+	//				if (!thirds.contains(third) && flag) {
+	//					thirds.add(third);
+	//				}
+	//			}
+	//			second.setChildren(thirds);
+	//
+	//			// 一级菜单中不包含二级时添加
+	//			if (!first.getChildren().contains(second)) {
+	//				first.getChildren().add(second);
+	//			}
+	//		}
+	//	}
+	//	return vos;
+	//}
 }
